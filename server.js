fdaacacfd405c29a1c1a14f2ceb08158c317fb25
Alex Yuan <alexyuan@Alexs-MacBook-Pro.local>
2017-05-05 13:32:29 -0400

    burgers set up, need to add design

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 
-// var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
-// app.use("/", routes);
+app.use("/", routes);
 
 app.listen(port);
 
