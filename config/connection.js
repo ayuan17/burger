4a3dc4102a6c6ddaec2fb1fd9224dad00d8f3832
Alex Yuan <alexyuan@Alexs-MacBook-Pro.local>
2017-05-05 13:36:07 -0400

    jawsdb included in connection

--- config/connection.js
+++ config/connection.js
@@ -1,58 +1,62 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  connection = mysql.createConnection({
     port: 3306,
     host: 'localhost',
     user: 'root',
     password: 'helloalex',
     database: 'burgers_db'
-});
+  });
+};
 
 
+// Make connection.
 connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
+// Export connection for our ORM to use.
 module.exports = connection;
 
 
 
 
-
-
-
-
-
-// var mysql = require("mysql");
-
-// var connection;
-
-// if (process.env.JAWSDB_URL) {
-
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-// } else {
-//   connection = mysql.createConnection({
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
 //     port: 3306,
 //     host: 'localhost',
 //     user: 'root',
 //     password: 'helloalex',
 //     database: 'burgers_db'
-//   });
-// };
-
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
 // });
 
-// // Export connection for our ORM to use.
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+//     console.log('connected as id ' + connection.threadId);
+// });
+
 // module.exports = connection;
+
+
+
+
+
+
+
+
+
