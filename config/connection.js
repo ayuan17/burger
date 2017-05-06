c9425aa37793a94ae16dade76a5588bd82be3739
Alex Yuan <alexyuan@lawn-128-61-29-141.lawn.gatech.edu>
2017-05-06 09:57:03 -0400

    test2

--- config/connection.js
+++ config/connection.js
@@ -17,46 +17,9 @@ if (process.env.JAWSDB_URL) {
 };
 
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 // Export connection for our ORM to use.
 module.exports = connection;
-
-
-
-
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     port: 3306,
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'helloalex',
-//     database: 'burgers_db'
-// });
-
-
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error('error connecting: ' + err.stack);
-//         return;
-//     }
-//     console.log('connected as id ' + connection.threadId);
-// });
-
-// module.exports = connection;
-
-
-
-
-
-
 
 
 
