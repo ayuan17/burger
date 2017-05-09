be50ee1bd288438a58e23402c1995ab0de620dd0
Alex Yuan <alexyuan@lawn-128-61-29-141.lawn.gatech.edu>
2017-05-09 17:54:51 -0400

    heroku push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -28,4 +28,4 @@ app.use("/", routes);
 
 app.listen(port);
 
-console.log('App listening on PORT ' + port);
+console.log('App listening on port %s ' , port);
